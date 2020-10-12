7e7bfb58e2b0bbbe925c94353c4289054a33db93
kirill <031kirill2a@mail.ru>
2020-10-12 19:58:31 +0300

    add comments

--- src/ru/vsu/cs/ivanov/Main.java
+++ src/ru/vsu/cs/ivanov/Main.java
@@ -12,23 +12,24 @@ public class Main {
     }
 
     private static void drawFigure(int s) {
+        int longOfRepeatedCharacters = (s - 4) / 3;
         for (int i = 0; i < s; i++) {
             if ((i == 0) || (i == (s - 1))) { //условие для первой и последней строки
-                drawString (s, ' ', ' ', '*', '_');
+                drawString(s, ' ', ' ', '*', '_');
             }
-            if (((i > 0) && (i < (s - 4) / 3 + 1)) || (i < (s - 1) && (i > ((s - 1) - (s - 4) / 3 - 1)))) { //условие для строк c "|"
-                drawString (s, ' ', ' ', '|', ' ');                              //в верхней и нижней части
+            if (((i > 0) && (i < longOfRepeatedCharacters + 1)) || (i < (s - 1) && (i > ((s - 1) - longOfRepeatedCharacters - 1)))) { //условие для строк c "|"
+                drawString(s, ' ', ' ', '|', ' ');                              //в верхней и нижней части
             }
-            if (i == ((s - 4) / 3 + 1) || (i == ((s - 1) - (s - 4) / 3 - 1))) { //условие для строк с "*" в средней части
-                drawString (s, '*', '_', '*', ' ');
+            if (i == (longOfRepeatedCharacters + 1) || (i == ((s - 1) - longOfRepeatedCharacters - 1))) { //условие для строк с "*" в средней части
+                drawString(s, '*', '_', '*', ' ');
             }
-            if ((i > ((s - 4) / 3 + 1)) && (i < ((s - 1) - (s - 4) / 3 - 1))) { //условие для строк с "|" в средней части
-                drawString (s, '|', ' ', ' ', ' ');
+            if ((i > (longOfRepeatedCharacters + 1)) && (i < ((s - 1) - longOfRepeatedCharacters - 1))) { //условие для строк с "|" в средней части
+                drawString(s, '|', ' ', ' ', ' ');
             }
         }
     }
 
-    private static void drawString (int s, char char1, char char2, char char3, char char4) {
+    private static void drawString(int s, char char1, char char2, char char3, char char4) {
         System.out.print(char1);            //выводит самый левый символ
         drawRepeatedCharacters (s, char2);  //выводит левую длинную послеовательность символов
         System.out.print(char3);            //выводит средний левый символ
@@ -38,7 +39,7 @@ public class Main {
         System.out.println(char1);          //выводит самый правый символ
     }
 
-    private static void drawRepeatedCharacters (int s, char character) { //выводит длинные последовательности повторяющихся символов
+    private static void drawRepeatedCharacters(int s, char character) { //выводит длинные последовательности повторяющихся символов
         for (int i = 0; i < ((s - 4) / 3); i++) {
             System.out.print(character);
         }
